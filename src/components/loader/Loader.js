bbc8c7192640a7b1d39c155bc9614797fdb7a4b1
Bogdan Lytvyn <72852244+BogdanLytvyn@users.noreply.github.com>
2021-03-24 17:27:52 +0200

    fix loader

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -6,7 +6,14 @@ import styles from './Loader.module.css';
 export default class Loader extends Component {
   render() {
     return (
-      <Spinner type="ThreeDots" color="#00BFFF" height={50} width={50} className={styles.Spinner} />
+      <Spinner
+        type="ThreeDots"
+        color="#00BFFF"
+        height={50}
+        width={50}
+        className={styles.Spinner}
+        timeout={500}
+      />
     );
   }
 }
